2c0a002ed4772c8ff67ea09a201ddae2676e116f
janvda <jan@Jans-MBP.lan>
2020-11-28 12:48:57 +0100

    set status after edge impulse is loaded

--- edge-impulse-classify.js
+++ edge-impulse-classify.js
@@ -9,86 +9,92 @@ module.exports = function(RED) {
         this.path       = config.path;
         this.edge_impulse_module = this.path + "/edge-impulse-standalone";
 
-        // set node status based on edge impulse existing on path
-        if (fs.existsSync(this.edge_impulse_module + ".js")){
-            this.status({fill:"green",shape:"dot",text:"edge impulse found" });
-        } else {
-            this.status({fill:"red",shape:"ring",text:"no edge impulse at path:" + this.edge_impulse_module + ".js"});
-        }
+        /** A copy of 'this' object in case we need it in context of callbacks of other functions.*/
+        const node = this;
 
-        // based on https://docs.edgeimpulse.com/docs/through-webassembly
+        try {
+            if (!(fs.existsSync(this.edge_impulse_module + ".js"))){ throw("no edge impulse add path"); }
 
-        // Load the inferencing WebAssembly module
-        let Module = require( this.edge_impulse_module);
+            this.status({fill:"yellow",shape:"dot",text:"loading edge-impulse:" + this.edge_impulse_module });
 
-        // Classifier module
-        let classifierInitialized = false;
-        Module.onRuntimeInitialized = function() {
-            classifierInitialized = true;
-        };
+            // based on https://docs.edgeimpulse.com/docs/through-webassembly
 
-        class EdgeImpulseClassifier {
-            _initialized = false;
+            // Load the inferencing WebAssembly module
+            let Module = require( this.edge_impulse_module);
 
-            init() {
-                if (classifierInitialized === true) return Promise.resolve();
+            // Classifier module
+            let classifierInitialized = false;
+            Module.onRuntimeInitialized = function() {
+                classifierInitialized = true;
+            };
 
-                return new Promise((resolve) => {
-                    Module.onRuntimeInitialized = () => {
-                        resolve();
-                        classifierInitialized = true;
+            class EdgeImpulseClassifier {
+                _initialized = false;
+
+                init() {
+                    if (classifierInitialized === true) return Promise.resolve();
+
+                    return new Promise((resolve) => {
+                        Module.onRuntimeInitialized = () => {
+                            resolve();
+                            classifierInitialized = true;
+                            node.status({fill:"green",shape:"dot",text:"successfully loaded edge-impulse:" + node.edge_impulse_module });
+                        };
+                    });
+                }
+
+                classify(rawData, debug = false) {
+                    if (!classifierInitialized) throw new Error('Module is not initialized');
+
+                    const obj = this._arrayToHeap(rawData);
+                    let ret = Module.run_classifier(obj.buffer.byteOffset, rawData.length, debug);
+                    Module._free(obj.ptr);
+
+                    if (ret.result !== 0) {
+                        throw new Error('Classification failed (err code: ' + ret.result + ')');
+                    }
+
+                    let jsResult = {
+                        anomaly: ret.anomaly,
+                        results: []
                     };
-                });
-            }
 
-            classify(rawData, debug = false) {
-                if (!classifierInitialized) throw new Error('Module is not initialized');
+                    for (let cx = 0; cx < ret.classification.size(); cx++) {
+                        let c = ret.classification.get(cx);
+                        jsResult.results.push({ label: c.label, value: c.value });
+                    }
 
-                const obj = this._arrayToHeap(rawData);
-                let ret = Module.run_classifier(obj.buffer.byteOffset, rawData.length, debug);
-                Module._free(obj.ptr);
-
-                if (ret.result !== 0) {
-                    throw new Error('Classification failed (err code: ' + ret.result + ')');
+                    return jsResult;
                 }
 
-                let jsResult = {
-                    anomaly: ret.anomaly,
-                    results: []
-                };
-
-                for (let cx = 0; cx < ret.classification.size(); cx++) {
-                    let c = ret.classification.get(cx);
-                    jsResult.results.push({ label: c.label, value: c.value });
+                _arrayToHeap(data) {
+                    let typedArray = new Float32Array(data);
+                    let numBytes = typedArray.length * typedArray.BYTES_PER_ELEMENT;
+                    let ptr = Module._malloc(numBytes);
+                    let heapBytes = new Uint8Array(Module.HEAPU8.buffer, ptr, numBytes);
+                    heapBytes.set(new Uint8Array(typedArray.buffer));
+                    return { ptr: ptr, buffer: heapBytes };
                 }
-
-                return jsResult;
             }
 
-            _arrayToHeap(data) {
-                let typedArray = new Float32Array(data);
-                let numBytes = typedArray.length * typedArray.BYTES_PER_ELEMENT;
-                let ptr = Module._malloc(numBytes);
-                let heapBytes = new Uint8Array(Module.HEAPU8.buffer, ptr, numBytes);
-                heapBytes.set(new Uint8Array(typedArray.buffer));
-                return { ptr: ptr, buffer: heapBytes };
-            }
+            // Initialize the classifier
+            let classifier = new EdgeImpulseClassifier();
+            classifier.init()
+
+            this.on('input', function(msg) {
+                msg.payload = classifier.classify(msg.payload);
+                this.send(msg);
+            });
+
+            this.on('close', function() {
+                // unloading edge impulse module
+                var name = require.resolve(this.edge_impulse_module);
+                delete require.cache[name];
+            });
         }
-
-    // Initialize the classifier
-        let classifier = new EdgeImpulseClassifier();
-        classifier.init()
-
-        this.on('input', function(msg) {
-            msg.payload = classifier.classify(msg.payload);
-            this.send(msg);
-        });
-
-        this.on('close', function() {
-            // unloading edge impulse module
-            var name = require.resolve(this.edge_impulse_module);
-            delete require.cache[name];
-        });
+        catch(err){
+            this.status({fill:"red",shape:"ring",text:"no edge impulse at path:" + this.edge_impulse_module + ".js"}); 
+        }
     }
     RED.nodes.registerType("edge-impulse-classify",EdgeImpulseClassifyNode);
 }
